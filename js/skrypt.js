3566313f71cf4a1715f41a7eaf04cc17f890780b
Leszek <lslowikowski@gmail.com>
2020-11-14 14:24:27 +0100

    Dodanie przycisku
    Dodanie przycisku i komunikatu

--- js/skrypt.js
+++ js/skrypt.js
@@ -1,3 +1,6 @@
 window.onload = function(){
-
+var przyciskInformacja = document.getElementById("informacja");
+ przyciskInformacja.addEventListener("click", function(){
+ alert("GitKraken to przyjazny program!");
+ });
 }
